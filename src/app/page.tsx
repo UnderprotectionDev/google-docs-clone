1aa522296f4d20bf69b05a389b739966dcab7999
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-21 15:25:08 +0300

    Replace button in Home component with a link to documents and remove unused sign-in and sign-up pages
    - Updated Home component to include a link instead of a button for better navigation.
    - Deleted obsolete sign-in and sign-up pages to streamline the authentication process.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center ">
-      <Button variant={"destructive"}>Click me</Button>
+      Click{" "}
+      <Link href="/documents/123" className="text-blue-500 underline">
+        here
+      </Link>
     </div>
   );
 }
