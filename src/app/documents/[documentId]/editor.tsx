89e4dc0d5bcad6e62ae12e934a1a65d0fceaa07f
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-24 13:19:14 +0300

    Add Underline extension to Tiptap editor and update Toolbar functionality
    - Included the Underline extension in package.json and package-lock.json for enhanced text formatting options.
    - Updated the Editor component to utilize the Underline extension.
    - Enhanced the Toolbar component to include an Underline button with associated functionality for toggling underline formatting.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
 import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
 import Table from "@tiptap/extension-table";
@@ -9,7 +11,6 @@ import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
-import StarterKit from "@tiptap/starter-kit";
 
 import { useEditorStore } from "@/store/use-editor-store";
 
@@ -50,6 +51,7 @@ export function Editor() {
     },
     extensions: [
       StarterKit,
+      Underline,
       Image,
       ImageResize,
       Table,
