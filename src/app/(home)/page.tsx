ad5ef99f6b9c578bdd46104467785b9144ac9f8d
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-30 02:44:17 +0300

    Add Convex integration and update layout and Home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./templates-gallery";
+import { api } from "../../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments);
+
+  if (documents === undefined) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex min-h-screen flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +19,9 @@ export default function Home() {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <div key={document._id}>{document.title}</div>
+        ))}
       </div>
     </div>
   );
